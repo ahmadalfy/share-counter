6cb9a6799b042e419186db86fc5befb4e37a1d4e
Ahmad Alfy <ahmadalfy@gmail.com>
2017-08-20 14:49:17 +0200

    Add Facebook counter

---
+++ src/channels/facebook.js
@@ -0,0 +1,21 @@
+// @flow
+
+export default function FacebookCounter(a:HTMLAnchorElement) {
+  let link = a.href;
+  link = link.split('?u=')[1];
+
+  function updateDOM() {
+    const response = JSON.parse(this.responseText);
+    if (response.error) {
+      console.warn('Error getting Facebook share count:', response.error.message);
+      return;
+    }
+    const count = document.createElement('span');
+    count.textContent = response.share.share_count;
+    a.append(count);
+  }
+  const oReq = new XMLHttpRequest();
+  oReq.addEventListener('load', updateDOM);
+  oReq.open('GET', `https://graph.facebook.com/?id=${encodeURIComponent(link)}`);
+  oReq.send();
+}
--- src/share-counter.js
+++ src/share-counter.js
@@ -1,1 +1,28 @@
 // @flow
+
+import FacebookCounter from './channels/facebook';
+
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+function parseLinks() {
+  // https://stackoverflow.com/a/40053682/497828
+  const links = ((document.querySelectorAll('a[data-counter]'): any): NodeList<HTMLAnchorElement>);
+  links.forEach((link:HTMLAnchorElement) => {
+    switch (link.hostname) {
+      case 'www.facebook.com':
+      case 'facebook.com':
+        FacebookCounter(link);
+        break;
+      default:
+        break;
+    }
+  });
+}
+
+ready(parseLinks);
